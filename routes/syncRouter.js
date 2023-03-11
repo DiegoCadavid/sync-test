2c73e65dc9121ffe57d9953f9f0cf995f12bb61e
DiegoCadavid <dcmgames50@gmail.com>
2023-03-11 15:22:28 -0500

    feat: add log

--- routes/syncRouter.js
+++ routes/syncRouter.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const syncRouter = express.Router();
 
-syncRouter.get('/', (req, res) => {
-    res.status(200).json({ msg : 'path test'});
+syncRouter.get("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).send("ok");
 });
 
 module.exports = syncRouter;
